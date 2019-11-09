d1a8d1e9c69d4663b7591cbdd80eb09100a72350
artak-sahakyan <web.artaksahakyan@gmail.com>
2019-11-09 13:57:48 +0400

    fix autoload

--- public/index.php
+++ public/index.php
@@ -3,6 +3,7 @@ use controllers\AdminController;
 use controllers\TaskController;
 
 spl_autoload_register(function ($class) {
+    $class = str_replace('\\', '/', $class);
     require_once '../'.$class . '.php';
 });
 
